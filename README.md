57ad9e06691f6801c001713c89abdb02f79df648
Franco Narvaez <juanfranconarvaez@gmail.com>
2026-03-05 17:36:11 -0300

    Paso #2: Interpolacion

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # final_pwa_jfn_032025
-# final_pwa_jfn_032025
+
+## Pre Requisitos
+
+NodeJS >= v22.10
+
+## Instalacion
+
+1) Instalar dependencias
+```bash
+    cd app
+    npm install
+```
+2) Correr aplicacion
+```bash
+    npm run serve
+```
---
+++ app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+  <p>Nombre: {{ nombre }}</p>
+  <p>Edad: {{ edad }}</p>
+  <p>Ciudad: {{ ciudad }}</p>
+  <p>"Hola, mi nombre es {{ nombre }}, tengo {{ edad }} años y vivo en {{ ciudad }}."</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+      data() {
+        return {
+            nombre: "Ana García",
+            edad: 30,
+            ciudad: "Tupungato",    
+        }
+    },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
